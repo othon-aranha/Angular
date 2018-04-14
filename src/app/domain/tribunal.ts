d2aacfb893122d336bd0007a8219d6a4d67ae45c
Othon Aranha <othon.aranha@tse.jus.br>
2018-04-14 16:38:54 -0300

    correções do merge

--- src/app/domain/tribunal.ts
+++ src/app/domain/tribunal.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export class Tribunal {
-=======
-export class Tribunal{
->>>>>>> 8698d01a8fb4e36913402b6804d767fe2a9f294a
     id: number;
     sigla: string;
     nome: string;
@@ -19,8 +15,4 @@ export class Tribunal{
     codigoNaturezaJuridica: number;
     email: string;
     acesso: string;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 8698d01a8fb4e36913402b6804d767fe2a9f294a
