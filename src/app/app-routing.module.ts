82d54881faf4b4edeb31b04582900ab6cf68e82f
Othon Aranha <othonaranha@gmailcom>
2018-08-19 19:09:16 -0300

    Retirada de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { ModuloListComponent } from './modulo/modulo-list.component';
-import { UsuarioListComponent } from './usuario/usuario-list/usuario-list.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 import { UsuarioService } from './service/usuario.service';
-import { ModuloModule } from './modulo/modulo.module';
 
 
 const appRoutes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,11 +20,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ServidorListComponent } from './servidor-list/servidor-list.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
-import { ServidorService } from './service/servidor.service';
 import { ManutencaoService } from './service/manutencao.service';
 import { AuthService } from './service/auth.service';
 
